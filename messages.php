eae5069ee3a5cc31d015c2c06c93a4c63e65bd2b
Tom Appenzeller <tappenze@purdue.edu>
2020-01-03 21:54:49 -0500

    Formatted Message.php

--- messages.php
+++ messages.php
@@ -1,1 +1,10 @@
+<html>
+<body>
 
+Name: <?php echo $_POST["Name"]; ?><br>
+Email: <?php echo $_POST["Email"]; ?>
+Subject: <?php echo $_POST["Subject"]; ?>
+Message: <?php echo $_POST["Message"]; ?>
+
+</body>
+</html>
