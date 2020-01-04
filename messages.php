283188ea6c8db32c5a50929b22c1914c25e6a4f3
Tom Appenzeller <tappenze@purdue.edu>
2020-01-04 13:24:40 -0500

    PHP redirect to email

--- messages.php
+++ messages.php
@@ -1,10 +1,16 @@
-<html>
-<body>
+<?php
 
-Name: <?php echo $_POST["Name"]; ?><br>
-Email: <?php echo $_POST["Email"]; ?>
-Subject: <?php echo $_POST["Subject"]; ?>
-Message: <?php echo $_POST["Message"]; ?>
+  $to_email = 'tomapp3@gmail.com';
 
-</body>
-</html>
+  $name = $_POST['Name'];
+  $email = $_POST['Email'];
+  $subject = $_POST['Subject'];
+  $message = $_POST['Message'];
+
+  $total_message = "Name: " . $name . ", email: " . $email . ", message: " . $message;
+
+  mail($to_email, $subject, $total_message);
+
+  
+
+  header('Location: /');
