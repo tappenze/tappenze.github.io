03b2310189495153a16efca6bb5b8a5090325c6f
Tom Appenzeller <tappenze@purdue.edu>
2020-01-05 13:31:07 -0500

    messages.php redirect to my_messages.log

--- messages.php
+++ messages.php
@@ -1,15 +1,15 @@
 <?php
 
-  $to_email = 'tomapp3@gmail.com';
+  $log_file_name = 'my_messages.log';
 
   $name = $_POST['Name'];
   $email = $_POST['Email'];
   $subject = $_POST['Subject'];
   $message = $_POST['Message'];
 
-  $total_message = "Name: " . $name . ", email: " . $email . ", message: " . $message;
+  $total_message = "Name: " . $name . ", email: " . $email . ", Subject: " . $subject . ", message: " . $message;
 
-  mail($to_email, $subject, $total_message);
+  file_put_contents($log_file_name, $total_message, FILE_APPEND);
 
   
 
